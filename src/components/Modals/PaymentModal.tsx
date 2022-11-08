48d0e507ae68fd89004afe006b0e4ff5bdacdbac
oluwadamoo <mrdamilolasaliu@gmail.com>
2022-11-08 17:21:16 +0100

    add pay with wema script

--- src/components/Modals/PaymentModal.tsx
+++ src/components/Modals/PaymentModal.tsx
@@ -1,78 +1,81 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PayWithBankIcon from '../../assets/pay_with_bank.svg';
 import PayStackIcon from '../../assets/paystack.svg';
 import styles from "./GetStarted.module.scss";
 import PaymentDetails from "./PaymentDetails";
-import {useState} from 'react'
+import { useState } from 'react'
 import axios from "axios";
+import useScript from '../../hooks/useScript'
 
-const  PaymentModal = ({visible, totalAmount, userWhatsappNumber, userEmail}) => {
-    const [openBankDetails, setOpenBankDetails] = useState(false)
-const openModal = () =>{
-setOpenBankDetails(true)
-}
+const PaymentModal = ({ visible, totalAmount, userWhatsappNumber, userEmail }) => {
+  const [openBankDetails, setOpenBankDetails] = useState(false)
 
-const NEXT_PUBLIC_WEMA_API_KEY= "0d296a5575ec4a0895c80d065fa44801"
-const NEXT_PUBLIC_WEMA_BUSINESS_ID= "348fc803-6a69-4580-e0ce-08daa1211597"
+  const openModal = () => {
+    setOpenBankDetails(true)
+  }
 
-const payWithWema = () => {
- 
+  const { Alatpay } = useScript("https://alatpay-test.azurewebsites.net/js/alatpay.js", 'Alatpay')
+  const NEXT_PUBLIC_WEMA_API_KEY = "0d296a5575ec4a0895c80d065fa44801"
+  const NEXT_PUBLIC_WEMA_BUSINESS_ID = "348fc803-6a69-4580-e0ce-08daa1211597"
 
-  let popup = Alatpay.setup({
-    apiKey: NEXT_PUBLIC_WEMA_API_KEY,
-    businessId: NEXT_PUBLIC_WEMA_BUSINESS_ID,
-    email: userEmail,
-    phone: userWhatsappNumber,
-    metaData: null,
-    currency: "NGN",
-    amount: totalAmount,
+  console.log(Alatpay, "alat pay")
 
-    onTransaction: async function (response) {
-        },
+  const payWithWema = () => {
+    let popup = Alatpay?.setup({
+      apiKey: NEXT_PUBLIC_WEMA_API_KEY,
+      businessId: NEXT_PUBLIC_WEMA_BUSINESS_ID,
+      email: userEmail,
+      phone: userWhatsappNumber,
+      metaData: null,
+      currency: "NGN",
+      amount: totalAmount,
 
-    onClose: function () {
-      console.log("Payment gateway is closed");
-    },
-  });
+      onTransaction: async function (response) {
+      },
 
-  popup.show();
-};
+      onClose: function () {
+        console.log("Payment gateway is closed");
+      },
+    });
 
-async function payStackCheckOut(params) {
+    popup.show();
+  };
+
+  async function payStackCheckOut(params) {
     try {
-        let request = axios.get(
-          `https://checkout.paystack.com/xt0tvv3zvo9p57k`
-        );
-        let response = (await request).data;
-        if (response) {
-            console.log(response, "inserdnknfj.k.")
-        }
-      } catch (error) {
-        console.error(error);
+      let request = axios.get(
+        `https://checkout.paystack.com/xt0tvv3zvo9p57k`
+      );
+      let response = (await request).data;
+      if (response) {
+        console.log(response, "inserdnknfj.k.")
       }
-}  
-    if(!visible) return null;
-return (
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  if (!visible) return null;
+  return (
     <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
-        <div className="bg-white p-2 rounded w-100">
+      <div className="bg-white p-2 rounded w-100">
         <h1 className="font-bold text-center text-xl text-gray-700">
-        Select a Payment Method        </h1>
-        
+          Select a Payment Method        </h1>
 
-        <div className="flex hover:cursor-pointer" onClick={() => window.open( 'https://checkout.paystack.com/xt0tvv3zvo9p57k')}>
-          <div  className=""><img src={PayStackIcon} alt="" /></div>
+
+        <div className="flex hover:cursor-pointer" onClick={() => window.open('https://checkout.paystack.com/xt0tvv3zvo9p57k')}>
+          <div className=""><img src={PayStackIcon} alt="" /></div>
           <p>Pay with Paystack</p>
         </div>
         <div className="flex">
-        <div  onClick={openModal}><img src={PayWithBankIcon}  alt=""/></div>
+          <div onClick={payWithWema}><img src={PayWithBankIcon} alt="" /></div>
           <p>Pay with Bank Transfer</p>
         </div>
-        
+
       </div>
 
-      <PaymentDetails visible={openBankDetails}/>
+      <PaymentDetails visible={openBankDetails} />
     </div>
-)
+  )
 }
 
 
---
+++ src/hooks/useScript.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+const useScript = (url: string, name: string) => {
+    const [lib, setLib] = useState<any>({})
+
+    useEffect(() => {
+        const script = document.createElement('script');
+
+        script.src = url;
+        script.async = true;
+        script.onload = () => setLib({ [name]: window[name] })
+
+        document.body.appendChild(script);
+
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, [url]);
+
+    console.log(lib, "alat pay inside usescript")
+    return lib
+};
+
+export default useScript;
