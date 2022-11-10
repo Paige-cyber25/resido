8f0f19419d62b209c5080426075ec54bb1b63dbc
Patience <patience@ext.kobo360.com>
2022-11-10 15:54:15 +0100

    refactor: made design changes

--- src/pages/GetStarted/GetStarted.tsx
+++ src/pages/GetStarted/GetStarted.tsx
@@ -30,7 +30,7 @@ const GetStarted = () => {
   const [houseTypes, setHouseTypes] = useState([]);
   const [price, setPrice] = useState<any>({});
   const [showPaymentModal, setShowPaymentModal] = useState<any>(false);
-  const [paystackUrl, setPaystackUrl] = useState<string>()
+  const [paystackUrl, setPaystackUrl] = useState<string>();
 
   const isThisForm = (formStepIndex: any) => {
     return currentFormKey === formSteps[formStepIndex].index;
@@ -48,15 +48,13 @@ const GetStarted = () => {
     1: "House 7+",
     2: "D/SD House",
     3: "Townhouse",
-    4: "3+ Bed Apartment"
+    4: "3+ Bed Apartment",
   };
 
   const frequencyOptions = {
     2: "quarterly",
-    3: "yearly"
-  }
-
-  
+    3: "yearly",
+  };
 
   const openPaymentModal = () => setShowPaymentModal(true);
 
@@ -105,80 +103,78 @@ const GetStarted = () => {
     }
   }
 
-  const { handleSubmit, setFieldValue, isValid, values } =
-    useFormik({
-      initialValues: {
-        house_type: "",
-        location: "",
-        frequency: "",
-        amount: "",
-        name: "",
-        email: "",
-        whatsapp_number: "",
-        alt_whatsapp_number: "",
-        address: "",
-      },
-      validationSchema: Yup.object({
-        name: Yup.string().required("Please input your full name"),
-        email: Yup.string()
-          .email("Use valid format")
-          .required("Please input your email address"),
-        address: Yup.string().required("Please input your home address"),
-        whatsapp_number: Yup.number().required(
-          "Please input your whatsapp number"
-        ),
-      }),
-      async onSubmit(values: any) {
+  const { handleSubmit, setFieldValue, isValid, values } = useFormik({
+    initialValues: {
+      house_type: "",
+      location: "",
+      frequency: "",
+      amount: "",
+      name: "",
+      email: "",
+      whatsapp_number: "",
+      alt_whatsapp_number: "",
+      address: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Please input your full name"),
+      email: Yup.string()
+        .email("Use valid format")
+        .required("Please input your email address"),
+      address: Yup.string().required("Please input your home address"),
+      whatsapp_number: Yup.number().required(
+        "Please input your whatsapp number"
+      ),
+    }),
+    async onSubmit(values: any) {
+      if (isValid) {
+        setFormValues({ ...values });
 
-        if (isValid) {
-          setFormValues({ ...values });
-
-          let postData = {
-            name: values.name,
-            email: values.email,
-            location: values.location,
-            whatsapp_number: values.whatsapp_number,
-            alt_whatsapp_number: values.alt_whatsapp_number,
-            frequency: values.frequency,
-            house_type: values.house_type,
-            amount: price.price,
-          };
-          console.log(postData)
-          try {
-            const postRes = await registerUser({ params: postData });
-            if (postRes.data.user) {
-              setPaystackUrl(postRes.data.authorizationUrl)
-              openPaymentModal()
-            }
-          } catch ({ error }) {
-            console.error(error);
+        let postData = {
+          name: values.name,
+          email: values.email,
+          location: values.location,
+          whatsapp_number: values.whatsapp_number,
+          alt_whatsapp_number: values.alt_whatsapp_number,
+          frequency: values.frequency,
+          house_type: values.house_type,
+          amount: price.price,
+        };
+        console.log(postData);
+        try {
+          const postRes = await registerUser({ params: postData });
+          if (postRes.data.user) {
+            setPaystackUrl(postRes.data.authorizationUrl);
+            openPaymentModal();
           }
+        } catch ({ error }) {
+          console.error(error);
         }
-      },
-    });
-
-    const onSubmit = async () => {
-      let postData = {
-        name: values.name,
-        email: values.email,
-        location: values.location,
-        whatsapp_number: values.whatsapp_number,
-        alt_whatsapp_number: values.alt_whatsapp_number,
-        frequency: values.frequency,
-        house_type: values.house_type,
-        amount: price.price,
-      };
-  
-      try {
-        const postRes = await registerUser({ params: postData });
-        if (postRes.data.user) {
-          setPaystackUrl(postRes.data.authorizationUrl)
-          openPaymentModal()
-        }
-      } catch (error) {
-        console.error(error)
       }
+    },
+  });
+
+  const onSubmit = async () => {
+    let postData = {
+      name: values.name,
+      email: values.email,
+      location: values.location,
+      whatsapp_number: values.whatsapp_number,
+      alt_whatsapp_number: values.alt_whatsapp_number,
+      frequency: values.frequency,
+      house_type: values.house_type,
+      amount: price.price,
+    };
+
+    try {
+      const postRes = await registerUser({ params: postData });
+      if (postRes.data.user) {
+        setPaystackUrl(postRes.data.authorizationUrl);
+        openPaymentModal();
+      }
+    } catch (error) {
+      console.error(error);
     }
+  };
 
   let houseTypeObject: any = Object.assign({}, houseTypes);
 
@@ -218,9 +214,13 @@ const GetStarted = () => {
                 Which of this best describes your home?
               </p>
               <div className="grid grid-cols-2 auto-rows-[0, 2fr] lg:ml-[2rem] lg:gap-[1.75rem]"></div>
-              <div className={`flex flex-col lg:flex-row w-full justify-center pt-8 ml-[1rem] lg:ml-0` }>
+              <div
+                className={`flex flex-col lg:flex-row w-full justify-center pt-8 ml-[1rem] lg:ml-0`}
+              >
                 <div className={`w-[85%] lg:w-[45%] ${styles.homeContainer}`}>
-                  <div className={`w-[100%] lg:w-[95%] mb-[2rem] lg:mb-0 ${styles.divider}`}>
+                  <div
+                    className={`w-[100%] lg:w-[95%] mb-[2rem] lg:mb-0 ${styles.divider}`}
+                  >
                     <div className={`p-2 ${styles.inputField}`}>
                       <input
                         type="checkbox"
@@ -259,9 +259,13 @@ const GetStarted = () => {
                 </div>
               </div>
 
-              <div className={`inline-flex flex-col lg:flex-row w-full justify-center pt-8 ml-[1rem] lg:ml-0`}>
+              <div
+                className={`inline-flex flex-col lg:flex-row w-full justify-center pt-8 ml-[1rem] lg:ml-0`}
+              >
                 <div className={`w-[85%] lg:w-[45%] ${styles.homeContainer}`}>
-                  <div className={`w-[100%] lg:w-[95%] mb-[2rem] lg:mb-0 ${styles.divider}`}>
+                  <div
+                    className={`w-[100%] lg:w-[95%] mb-[2rem] lg:mb-0 ${styles.divider}`}
+                  >
                     <div className={`p-2 ${styles.inputField}`}>
                       <input
                         type="checkbox"
@@ -293,9 +297,7 @@ const GetStarted = () => {
                         setFieldValue("house_type", houseTypeObject?.[3]?.id)
                       }
                     />
-                    <label
-                      className={`text-dark_grey font-light pl-4 lg:pl-6`}
-                    >
+                    <label className={`text-dark_grey font-light pl-4 lg:pl-6`}>
                       {houseTypeObject?.[3]?.name}
                     </label>
                   </div>
@@ -312,10 +314,14 @@ const GetStarted = () => {
                 Let us know where you are
               </p>
               <p className={`p-4 font-normal text-dark_grey`}>Select</p>
-              <div className={`flex flex-col lg:flex-row w-full justify-center`}>
+              <div
+                className={`flex flex-col lg:flex-row w-full justify-center`}
+              >
                 <div className={`w-[85%] lg:w-[45%] ${styles.homeContainer}`}>
                   <div className={`w-[100%] lg:w-[95%] ${styles.divider}`}>
-                    <div className={`p-[1rem] lg:p-2 lg:p-6 ml-[2rem] lg:ml-0 mb-[2rem] ${styles.inputField}`}>
+                    <div
+                      className={`p-[1rem] lg:p-2 lg:p-6 ml-[2rem] lg:ml-0 mb-[2rem] ${styles.inputField}`}
+                    >
                       <label
                         className={`text-dark_grey font-light pl-1 lg:pl-6 mb-4`}
                       >
@@ -340,13 +346,19 @@ const GetStarted = () => {
                             );
                           })}
                         </select>
-                        <img src={selectDropDownIcon} alt="select-drop-down" className={`hidden lg:block`}/>
+                        <img
+                          src={selectDropDownIcon}
+                          alt="select-drop-down"
+                          className={`hidden lg:block`}
+                        />
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className={`w-[85%] lg:w-[45%] ${styles.homeContainer}`}>
-                  <div className={`p-[1rem] lg:p-2 lg:p-6 ml-[2rem] lg:ml-0  ${styles.inputField}`}>
+                  <div
+                    className={`p-[1rem] lg:p-2 lg:p-6 ml-[2rem] lg:ml-0  ${styles.inputField}`}
+                  >
                     <label className={`text-dark_grey font-light pl-6 mb-4`}>
                       Frequency
                     </label>
@@ -372,7 +384,11 @@ const GetStarted = () => {
                           );
                         })}
                       </select>
-                      <img src={selectDropDownIcon} alt="select-drop-down" className={`hidden lg:block`} />
+                      <img
+                        src={selectDropDownIcon}
+                        alt="select-drop-down"
+                        className={`hidden lg:block`}
+                      />
                     </div>
                   </div>
                 </div>
@@ -419,11 +435,21 @@ const GetStarted = () => {
       {isThisForm(1) && (
         <>
           <div className={``}>
-            <h5
-              className={`text-center lg:text-right lg:pr-64 pt-10 text-dark_grey font-medium lg:mt-20 text-base`}
-            >
-              Fields marked * are compulsory
-            </h5>
+            <div className={`flex items-center justify-between`}>
+              <h5
+                onClick={renderPreviousForm}
+                className={`text-left lg:text-left cursor-pointer pl-[3rem] lg:pl-[16rem] pt-10 text-dark_grey font-medium lg:mt-20 text-base`}
+              >
+                Back
+              </h5>
+
+              <h5
+                className={`text-center lg:text-right lg:pr-64 pt-10 text-dark_grey font-medium lg:mt-20 text-base`}
+              >
+                Fields marked * are compulsory
+              </h5>
+            </div>
+
             <div
               className={` mx-[2rem] lg:mx-auto mt-4 lg:mt-2 lg:p-4 lg:mb-[2rem] ${styles.formContainer}`}
             >
@@ -522,7 +548,6 @@ const GetStarted = () => {
                       type="filled"
                       bgColor="light_blue"
                       color="dark_blue"
-
                       onClick={renderNextForm}
                       text="Submit"
                       classes="w-32 h-10 md:w-28 xl:w-36 cursor-pointer rounded-md text-sm capitalize text-dark_blue bg-light_blue"
@@ -538,13 +563,19 @@ const GetStarted = () => {
         <div>
           <div>
             <h5
-            onClick={renderPreviousForm}
+              onClick={renderPreviousForm}
               className={`text-left lg:text-left cursor-pointer pl-[3rem] lg:pl-[30rem] pt-10 text-dark_grey font-medium lg:mt-20 text-base`}
-            >Back</h5>
-            <div className={`mx-[2rem] lg:mx-auto mt-2  lg:mt-8 mb-8 ${styles.formContainerSummary}`}>
+            >
+              Back
+            </h5>
+            <div
+              className={`mx-[2rem] lg:mx-auto mt-2  lg:mt-8 mb-8 ${styles.formContainerSummary}`}
+            >
               <div className={`${styles.flexBox} ${styles.headerSummary}`}>
                 <h2>Total (Vat Inclusive)</h2>
-                <h2 className={`${styles.medium_text_bold}`}>₦{NumberWithComma(price.price)}</h2>
+                <h2 className={`${styles.medium_text_bold}`}>
+                  ₦{NumberWithComma(price.price)}
+                </h2>
               </div>
               <h2 className={`${styles.medium_text_bold}`}>Breakdown</h2>
               <div>
@@ -553,24 +584,23 @@ const GetStarted = () => {
                   <p className={`${styles.medium_text}`}>
                     {apartmentTypes[String(values.house_type)]}
                   </p>
-                  <p className={`${styles.medium_text}`}>₦ {NumberWithComma(price.price)}</p>
-                </div>
-              </div>
-
-              {frequencyOptions[String(values.frequency)] === frequencyOptions['3'] ? (
-                <div>
-                <h3 className={`${styles.medium_text}`}>Discount</h3>
-                <div className={`${styles.flexBox}`}>
-                  <p className={`${styles.medium_text} ${styles.discount}`}>
-                    10% off
+                  <p className={`${styles.medium_text}`}>
+                    ₦ {NumberWithComma(price.price)}
                   </p>
                 </div>
               </div>
-              )
-              : 
-              null
-            }
-              
+
+              {frequencyOptions[String(values.frequency)] ===
+              frequencyOptions["3"] ? (
+                <div>
+                  <h3 className={`${styles.medium_text}`}>Discount</h3>
+                  <div className={`${styles.flexBox}`}>
+                    <p className={`${styles.medium_text} ${styles.discount}`}>
+                      10% off
+                    </p>
+                  </div>
+                </div>
+              ) : null}
 
               <div className={`${styles.flexBox}`}>
                 <p className={`${styles.title_sub_text}`}>Location</p>
@@ -583,7 +613,9 @@ const GetStarted = () => {
               </div>
 
               <h4 className={`${styles.title_sub_text}`}>Frequency</h4>
-              <h3 className={`capitalize ${styles.medium_text}`}>{frequencyOptions[String(values.frequency)]}</h3>
+              <h3 className={`capitalize ${styles.medium_text}`}>
+                {frequencyOptions[String(values.frequency)]}
+              </h3>
 
               <button
                 type="submit"
@@ -596,7 +628,10 @@ const GetStarted = () => {
           </div>
         </div>
       )}
-      <PaymentModal {...{ totalAmount, userWhatsappNumber, userEmail, paystackUrl }} visible={showPaymentModal} />
+      <PaymentModal
+        {...{ totalAmount, userWhatsappNumber, userEmail, paystackUrl }}
+        visible={showPaymentModal}
+      />
     </form>
   );
 };
